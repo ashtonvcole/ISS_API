6c2be553a2f09d9a720ac914b10eeea231121ab9
ashtonvcole <ashtoncole1028@gmail.com>
2023-03-06 15:41:39 +0000

    Added new routes

--- iss_tracker.py
+++ iss_tracker.py
@@ -3,10 +3,15 @@
 from flask import Flask, request
 import requests
 import xmltodict
+from geopy.geocoders import Nominatim
+import math
+import time
 
 
 
 app = Flask(__name__)
+MEAN_EARTH_RADIUS = 6371
+data = None # Defined below
 
 
 
@@ -146,12 +151,107 @@ def epochs_speed(epoch: str) -> dict:
         return f'Empty data set\n', 404
     for item in data['ndm']['oem']['body']['segment']['data']['stateVector']:
         if item['EPOCH'] == epoch:
-            return {'speed' : (float(item['X_DOT']['#text']) ** 2 + \
+            return {'value' : (float(item['X_DOT']['#text']) ** 2 + \
                     float(item['Y_DOT']['#text']) ** 2 + \
                     float(item['Z_DOT']['#text']) ** 2) ** 0.5, \
                     'units' : item['X_DOT']['@units']}
     else: # No matching epoch
         return f'Epoch {epoch} not found\n', 404
+
+
+
+@app.route('/epochs/<string:epoch>/location', methods = ['GET'])
+def epochs_location(epoch: str) -> dict:
+    """Get location for a specified epoch.
+
+    This function takes in a string representing the desired epoch. If an
+    exact match is found in the data set, its calculated location is returned.
+    Otherwise, the user is given a 404 error.
+
+    Args:
+        String representing the desired epoch.
+
+            YYYY-DDDTHH:MM:SS.000Z, or in URL-friendly form
+            YYYY-DDDTHH%3AMM%3ASS%2E000Z
+
+    Returns:
+        A dictionary showing the closest identifiable location to the ISS's
+        ground track at a given epoch, as well as the latitude, longitude,
+        altitude, and speed. If there is an error, a descriptive string will
+        be returned with a 404 status code. If the ISS is not close to
+        anything, such as when its over the ocean, the location value will be
+        set to None.
+    """
+    global data, MEAN_EARTH_RADIUS
+    if data == None:
+        return f'Empty data set\n', 404
+    for item in data['ndm']['oem']['body']['segment']['data']['stateVector']:
+        if item['EPOCH'] == epoch:
+            x = float(item['X']['#text'])
+            y = float(item['Y']['#text'])
+            z = float(item['Z']['#text'])
+            hrs = int(item['EPOCH'][9:11])
+            mins = int(item['EPOCH'][12:14])
+            lat = math.degrees(math.atan2(z, math.sqrt(x**2 + y**2)))
+            lon = math.degrees(math.atan2(y, x)) \
+                    - ((hrs-12.0)+(mins/60.0))*(360.0/24.0) + 24.0
+            alt = math.sqrt(x**2 + y**2 + z**2) - MEAN_EARTH_RADIUS
+            geocoder = Nominatim(user_agent = 'iss_tracker')
+            geoloc = geocoder.reverse((lat, lon), zoom = 15, language = 'en')
+            to_return = {}
+            to_return['closest_epoch'] = item['EPOCH']
+            to_return['seconds_from_now'] = 0
+            to_return['location'] = {}
+            to_return['location']['latitude'] = lat
+            to_return['location']['longitude'] = lon
+            to_return['location']['altitude'] = {}
+            to_return['location']['altitude']['value'] = alt
+            to_return['location']['altitude']['units'] = item['X']['@units']
+            if geoloc == None:
+                to_return['location']['geo'] = None
+            else:
+                to_return['location']['geo'] = geoloc.raw
+            to_return['speed'] = epochs_speed(epoch)
+            return to_return
+    else: # No matching epoch
+        return f'Epoch {epoch} not found\n', 404
+
+
+
+@app.route('/now', methods = ['GET'])
+def now() -> dict:
+    """Get location for the current epoch.
+
+    This function returns the location of the ISS at the closest recorded
+    epoch. It provides the difference between this time and now in seconds.
+
+    Args:
+        None
+
+    Returns:
+        A dictionary showing the closest identifiable location to the ISS's
+        ground track at a given epoch, as well as the latitude, longitude,
+        altitude, and speed. If there is an error, a descriptive string will
+        be returned with a 404 status code. If the ISS is not close to
+        anything, such as when its over the ocean, the location geo value
+        will be set to None.
+    """
+    global data, MEAN_EARTH_RADIUS
+    time_now = time.time()
+    min_item = None
+    min_difference = float('inf')
+    if data == None:
+        return f'Empty data set\n', 404
+    for item in data['ndm']['oem']['body']['segment']['data']['stateVector']:
+        time_epoch = time.mktime(time.strptime( \
+                item['EPOCH'][:-5], '%Y-%jT%H:%M:%S'))
+        difference = time_now - time_epoch
+        if abs(difference) < abs(min_difference):
+            min_item = item
+            min_difference = difference
+    to_return = epochs_location(min_item['EPOCH'])
+    to_return['seconds_from_now'] = min_difference
+    return to_return
 
 
 
@@ -171,8 +271,10 @@ def delete_data() -> str:
     data = None
     return 'Data deleted from instance\n'
 
+
+
 @app.route('/post-data', methods = ['POST'])
-def post_data() -> None:
+def post_data() -> str:
     """Refresh API data.
 
     This function retrieves the latest ISS position data from the NASA
@@ -190,6 +292,63 @@ def post_data() -> None:
     global data
     data = get_data()
     return 'Data refreshed\n'
+
+
+
+@app.route('/comment', methods = ['GET'])
+def comment() -> list:
+    """Get comment object from data.
+
+    Args:
+        None
+
+    Returns:
+        A list of all of the comments included in the OEM. If there is an
+        error, a descriptive string will be returned with a 404 status code.
+    """
+    global data
+    if data == None:
+        return f'Empty data set\n', 404
+    return data['ndm']['oem']['body']['segment']['data']['COMMENT']
+
+
+
+@app.route('/header', methods = ['GET'])
+def header() -> dict:
+    """ Get header object from data.
+
+    Args:
+        None
+
+
+    Returns:
+        A dictionary of header data included in the OEM. This may include the
+        creation date and originator of the file. If there is an error, a
+        descriptive string will be returned with a 404 status code.
+    """
+    global data
+    if data == None:
+        return f'Empty data set\n', 404
+    return data['ndm']['oem']['header']
+
+
+
+@app.route('/metadata', methods = ['GET'])
+def metadata() -> dict:
+    """ Get orbit metadata.
+
+    Args:
+        None
+
+    Returns:
+        A dictionary of metadata associated with an oriting body. This
+        includes the object's identifiers and reference frame. If there is an
+        error, a descriptive string will be returned with a 404 status code.
+    """
+    global data
+    if data == None:
+        return f'Empty data set\n', 404
+    return data['ndm']['oem']['body']['segment']['metadata']
 
 
 
